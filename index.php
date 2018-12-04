9616ca122cca26b854eeece6bd6d034a920620e8
Killian974 <bellour.killian@gmail.com>
2018-12-04 11:44:47 +0100

    Création d'une class personn + propriété

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-var_dump(expression:"coucou");
+class Person {
+    private $firstname;
+    private $lastname;
+
+    public function setFirsname($pFirstname){
+        $this->firstname = $pFirstname;
+    }
+
+    public function setLastname($pLastname){
+        $this ->lastname = $pLastname;
+    }
+
+ }
+
+$person1 = new Person();
+ $person1 ->setFirsname("Jean");
+ $person1 ->setLastname("Martin");
+
+$person2 = new Person();
+ // $person1 ->setFirsname("Jeanne");
+ // $person1 ->setLastname("Martin");
+
+var_dump($person1);
+var_dump($person2);
